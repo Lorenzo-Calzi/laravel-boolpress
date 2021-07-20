4695bea8b2a6e542aa23ceef6f6fd6567f17b929
Lorenzo-Calzi <lorenzocalzi@gmail.com>
2021-07-20 14:32:58 +0200

    Aggiunta struttura esercizio

---
+++ database/migrations/2021_07_20_122359_create_posts_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('user_image');
+            $table->string('user_name');
+            $table->integer('followers');
+            $table->integer('publication_data');
+            $table->string('post_type');
+            $table->string('post_image');
+            $table->text('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
