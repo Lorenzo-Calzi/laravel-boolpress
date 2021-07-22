1071e44b4bd2fe1bee0df2a6bd184577350fa3aa
Lorenzo-Calzi <lorenzocalzi@gmail.com>
2021-07-22 18:35:43 +0200

    Aggiunta sezione about

--- resources/views/guest/about.blade.php
+++ resources/views/guest/about.blade.php
@@ -2,7 +2,32 @@
 
 @section('content')
 
-    <div class="col-md-4 mb-3">
+    <div class="container">
         <h1>About Us</h1>
+        <div class="sections">
+
+            <div class="col-md-4 mb-3 section">
+                <h3>CONTACT</h3>
+                <h5>Via Roma 10</h5>
+                <h5>20019 MI Milano</h5>
+                <h5>024591784</h5>
+                <h5>contact@gmail.com</h5>
+            </div>
+
+            <div class="col-md-4 mb-3 section">
+                <h3>EXPERTISES</h3>
+                <h5>Mobility</h5>
+                <h5>Loyalty</h5>
+                <h5>Digital Twin</h5>
+            </div>
+
+            <div class="col-md-4 mb-3 section">
+                <i class="fab fa-facebook-f"></i>
+                <i class="fab fa-twitter"></i>
+                <i class="fab fa-linkedin-in"></i>
+                <i class="fab fa-instagram"></i> 
+            </div>
+        </div>
     </div>
+
 @endsection
