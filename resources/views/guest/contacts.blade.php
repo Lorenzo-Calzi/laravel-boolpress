095e1825495b73204995347b46ef9344d1ed2268
Lorenzo-Calzi <lorenzocalzi@gmail.com>
2021-07-23 21:44:36 +0200

    Correzioni form contact

--- resources/views/guest/contacts.blade.php
+++ resources/views/guest/contacts.blade.php
@@ -20,7 +20,7 @@
         </script>
     @endif
 
-    {{-- <form action="{{route('contacts.send')}}" method="post">
+    <form action="{{route('contacts.send')}}" method="post">
         @csrf
         <div class="form-group">
           <label for="">Full name</label>
@@ -41,62 +41,14 @@
           <textarea class="form-control" name="message" id="message" rows="5">{{old('message')}}</textarea>
         </div>
 
-        <button type="submit"class="btn btn-primary btn-block">📧 Send</button>
+        <div class="form-check mb-3">
+            <input class="form-check-input" type="checkbox" value="" id="invalidCheck2" required>
+            <label class="form-check-label" for="invalidCheck2">
+            Agree to terms and conditions
+            </label>
+        </div>
+
+        <button type="submit"class="btn btn-primary btn-block">Send</button>
     </form>
 </div>
- --}}
-
-
-<form action="{{route('contacts.send')}}" method="post">
-    @csrf
-
-    <div class="form-group">
-        <h2>Contact Us</h2>
-    </div>
-
-    <div>
-        <div class="form-group">
-            <label for="validationDefault01">First name</label>
-            <input type="text" class="form-control" id="validationDefault01" placeholder="First name" value="" required>
-        </div>
-
-        <div class="form-group">
-            <label for="validationDefault01">Last name</label>
-            <input type="text" class="form-control" id="validationDefault01" placeholder="Last name" value="" required>
-        </div>
-
-        <div class="form-group">
-            <label for="validationDefaultUsername">Email</label>
-            <div class="input-group">
-                <div class="input-group-prepend">
-                    <span class="input-group-text" id="inputGroupPrepend2">@</span>
-                 </div>
-                <input type="text" class="form-control" id="validationDefaultUsername" placeholder="Username" aria-describedby="inputGroupPrepend2" required>
-            </div>
-        </div>
-
-        <div class="form-group">
-            <label for="validationDefaultUsername">Comment</label>
-            <div class="input-group">
-                <div class="input-group-prepend">
-                <span class="input-group-text">Text</span>
-                </div>
-                <textarea class="form-control" aria-label="With textarea" placeholder="Comment"></textarea>
-            </div>
-        </div>
-        
-        <div class="form-group">
-            <div class="form-check">
-                <input class="form-check-input" type="checkbox" value="" id="invalidCheck2" required>
-                <label class="form-check-label" for="invalidCheck2">
-                Agree to terms and conditions
-                </label>
-            </div>
-        </div>
-
-        <div class="form-group">
-            <button class="btn btn-primary" type="submit">Submit form</button>   
-        </div>
-    </div>
-</form>
 @endsection
